c37c883731a2f9d65aee990ee2ca993819c13e88
Open legacy <openlegacy2@gmail.com>
2011-11-07 13:55:11 +0200

    *** fixed context file

--- openlegacy-tests/src/test/java/org/openlegacy/tools/StartServer.java
+++ openlegacy-tests/src/test/java/org/openlegacy/tools/StartServer.java
@@ -9,7 +9,7 @@ public class StartServer {
 
 	public static void main(String[] args) throws IOException {
 		RequestMockUtil.initRequest();
-		new ClassPathXmlApplicationContext("classpath*:/test-common-context.xml");
+		new ClassPathXmlApplicationContext("classpath*:/test-host-context.xml");
 
 		System.out.println("Press any key stop");
 		System.in.read();
