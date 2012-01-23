c765976d2d6c0a82ec17735b5d302b9701a1d474
Open legacy <openlegacy2@gmail.com>
2012-01-23 22:39:58 +0200

    refers #10 - re-commit basic eclipse plug-in as Maven module

---
+++ openlegacy/openlegacy-ide-eclipse/src/main/java/org/openlegacy/ide/eclipse/PluginConstants.java
@@ -0,0 +1,7 @@
+package org.openlegacy.ide.eclipse;
+
+public class PluginConstants {
+
+	public static final String PROJECT_FILE = ".project";
+
+}
---
+++ openlegacy/openlegacy-ide-eclipse/src/main/java/org/openlegacy/ide/eclipse/wizards/OpenLegacyeNewProjectWizard.java
@@ -0,0 +1,84 @@
+package org.openlegacy.ide.eclipse.wizards;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWizard;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
+import org.openlegacy.designtime.mains.DesignTimeExecuter;
+
+public class OpenLegacyeNewProjectWizard extends BasicNewResourceWizard {
+
+	private final static Logger logger = Logger.getLogger(OpenLegacyNewWizardPage.class);
+
+	private OpenLegacyNewWizardPage page;
+	private ISelection selection;
+
+	public OpenLegacyeNewProjectWizard() {
+		super();
+		setNeedsProgressMonitor(true);
+	}
+
+	@Override
+	public void addPages() {
+		page = new OpenLegacyNewWizardPage(selection);
+		addPage(page);
+	}
+
+	@Override
+	public boolean performFinish() {
+		final String templateName = page.getTemplateName();
+		final String projectName = page.getProjectName();
+
+		IPath workspacePath = ResourcesPlugin.getWorkspace().getRoot().getLocation();
+
+		IWorkspace w = ResourcesPlugin.getWorkspace();
+		final IProject projHandle = w.getRoot().getProject(projectName);
+		final IProjectDescription desc = w.newProjectDescription(projHandle.getName());
+
+		WorkspaceModifyOperation op = new WorkspaceModifyOperation() {
+
+			@Override
+			protected void execute(IProgressMonitor monitor) throws CoreException {
+				try {
+					projHandle.create(desc, monitor);
+					projHandle.open(IResource.BACKGROUND_REFRESH, new SubProgressMonitor(monitor, 1000));
+				} finally {
+					monitor.done();
+				}
+
+			}
+		};
+		try {
+			getContainer().run(true, true, op);
+			new DesignTimeExecuter().createProject(templateName, workspacePath.toOSString(), projectName);
+		} catch (Exception e) {
+			logger.fatal(e.getMessage(), e);
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * We will accept the selection in the workbench to see if we can initialize from it.
+	 * 
+	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
+	 */
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		this.selection = selection;
+	}
+
+}
