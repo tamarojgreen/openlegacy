03bfcaf10f4f70052da9b51c4d7bc4ec770e6509
Open legacy <openlegacy2@gmail.com>
2012-02-28 12:05:25 +0200

    refers #147 - css fix and added focus support in html emulation

--- openlegacy/openlegacy-templates/openlegacy-mvc-new/src/main/resources/META-INF/web-resources/openlegacy.html_emulation.js
+++ openlegacy/openlegacy-templates/openlegacy-mvc-new/src/main/resources/META-INF/web-resources/openlegacy.html_emulation.js
@@ -7,3 +7,13 @@ function TerminalSession() {
 }
 
 var terminalSession = new TerminalSession();
+
+dojo.addOnLoad(function(){
+    var elements = document.HtmlEmulation.elements;
+    for (var i=0;i<elements.length;i++){
+    	var element = elements[i];
+    	dojo.connect(element, "onfocus", function(e) {
+    		document.HtmlEmulation.TerminalCursor.value = e.target.name; 
+    	});
+    }
+});
