ccb0ed0830b83e1afd620b27b4502f5909afe0e7
Open legacy <openlegacy2@gmail.com>
2012-02-28 15:16:06 +0200

    refers #147 - - Fixed request URL mappings (JS + MVC params) - Align CSS. container is now static so the screen panels are in proportions

--- openlegacy/openlegacy-templates/openlegacy-mvc-new/src/main/resources/META-INF/web-resources/openlegacy.mvc.js
+++ openlegacy/openlegacy-templates/openlegacy-mvc-new/src/main/resources/META-INF/web-resources/openlegacy.mvc.js
@@ -1,6 +1,6 @@
 function doPost(actionName){
 	if (actionName != null && actionName.length > 0){
-		document.openlegacyForm.action = location.href + "?" + actionName;
+		document.openlegacyForm.action = location.href + "?action=" + actionName;
 	}
 	else{
 		document.openlegacyForm.action = location.href;
