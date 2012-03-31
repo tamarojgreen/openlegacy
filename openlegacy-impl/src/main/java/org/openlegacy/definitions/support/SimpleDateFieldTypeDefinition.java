d31ea4aa48b6e4a575f75e8f84095521cfa22788
Open legacy <openlegacy2@gmail.com>
2012-03-31 22:24:18 +0300

    refers #153 - identify date field - Added one rule for __ / __ / __ date field - Converted to @ScreenDateField(dayColumn=X,monthColumn=X,yearColumn=X)

---
+++ openlegacy-impl/src/main/java/org/openlegacy/definitions/support/SimpleDateFieldTypeDefinition.java
@@ -0,0 +1,33 @@
+package org.openlegacy.definitions.support;
+
+import org.openlegacy.definitions.DateFieldTypeDefinition;
+
+public class SimpleDateFieldTypeDefinition implements DateFieldTypeDefinition {
+
+	private Integer dayColumn;
+	private Integer monthColumn;
+	private Integer yearColumn;
+
+	public SimpleDateFieldTypeDefinition(Integer dayColumn, Integer monthColumn, Integer yearColumn) {
+		this.dayColumn = dayColumn;
+		this.monthColumn = monthColumn;
+		this.yearColumn = yearColumn;
+	}
+
+	public String getTypeName() {
+		return "date";
+	}
+
+	public Integer getYearColumn() {
+		return yearColumn;
+	}
+
+	public Integer getMonthColumn() {
+		return monthColumn;
+	}
+
+	public Integer getDayColumn() {
+		return dayColumn;
+	}
+
+}
