654edca7ff640055c0b585feebab14e4b9d476cc
Open legacy <openlegacy2@gmail.com>
2012-04-07 00:13:54 +0300

    refers #196 - end to end on app inventory sample app - Identify parent and child screens for item details 1,2 screens - Added Record Selection screen entity type - used to avoid defining parent/child from a record selection screen. - Push updated screen entities after finalize the screen names

--- openlegacy-api/src/main/java/org/openlegacy/modules/table/LookupEntity.java
+++ openlegacy-api/src/main/java/org/openlegacy/modules/table/LookupEntity.java
@@ -2,6 +2,12 @@ package org.openlegacy.modules.table;
 
 import org.openlegacy.EntityType;
 
+/**
+ * Represents a lookup window table entity type, used for selecting a value from a list
+ * 
+ * @author RoiM
+ * 
+ */
 public class LookupEntity implements EntityType {
 
 }
---
+++ openlegacy-api/src/main/java/org/openlegacy/modules/table/RecordSelectionEntity.java
@@ -0,0 +1,13 @@
+package org.openlegacy.modules.table;
+
+import org.openlegacy.EntityType;
+
+/**
+ * Represents a table entity type which is used for performing actions on it's records
+ * 
+ * @author RoiM
+ * 
+ */
+public class RecordSelectionEntity implements EntityType {
+
+}
