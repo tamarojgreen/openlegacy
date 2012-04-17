dd639a4e3f4e8180f7c7da488db40f6d5bdeb4ce
Open legacy <openlegacy2@gmail.com>
2012-04-18 00:09:25 +0300

    refers #196 - Added OL exception handler to properly disconnect the session in case of SessionEndedException - Increase the size of trail to unlimited when working in design-time - Changed default session history count to 3 - Re-factored trail controller class name, and added support for basic trail view

--- openlegacy-web/src/main/java/org/openlegacy/terminal/web/mvc/TrailController.java
+++ openlegacy-web/src/main/java/org/openlegacy/terminal/web/mvc/TrailController.java
@@ -5,7 +5,11 @@ import org.openlegacy.modules.trail.SessionTrail;
 import org.openlegacy.modules.trail.Trail;
 import org.openlegacy.modules.trail.TrailWriter;
 import org.openlegacy.terminal.TerminalSession;
+import org.openlegacy.terminal.TerminalSnapshot;
+import org.openlegacy.terminal.web.render.TerminalSnapshotHtmlRenderer;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.ByteArrayOutputStream;
@@ -19,13 +23,16 @@ import javax.servlet.http.HttpServletResponse;
  * Handles requests for the application home page.
  */
 @Controller
-public class SaveTrailController {
+public class TrailController {
 
 	@Inject
 	private TerminalSession terminalSession;
 
 	@Inject
 	private TrailWriter trailWriter;
+
+	@Inject
+	private TerminalSnapshotHtmlRenderer snapshotHtmlRenderer;
 
 	@RequestMapping(value = "/trail/download")
 	public void download(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -39,5 +46,15 @@ public class SaveTrailController {
 		response.setHeader("Content-Disposition",
 				MessageFormat.format("attachment; filename=\"{0}.trail.xml\"", terminalSession.getSessionId()));
 		response.getOutputStream().write(baos.toByteArray());
+	}
+
+	@RequestMapping(value = "/trail/{id}")
+	public String showSnapshot(@PathVariable("id") int id, Model uiModel) throws Exception {
+		SessionTrail<? extends Snapshot> trail = terminalSession.getModule(Trail.class).getSessionTrail();
+		Snapshot snapshot = trail.getSnapshots().get(id);
+
+		String result = snapshotHtmlRenderer.render((TerminalSnapshot)snapshot);
+		uiModel.addAttribute("terminalHtml", result);
+		return "HtmlEmulation";
 	}
 }
