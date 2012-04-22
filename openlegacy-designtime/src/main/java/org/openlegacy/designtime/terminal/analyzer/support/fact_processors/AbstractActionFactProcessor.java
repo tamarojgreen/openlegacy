052da2ecf12e5454cfa6a68ae5bd0932ba823e88
Open legacy <openlegacy2@gmail.com>
2012-04-23 00:05:17 +0300

    fixes #231 & refers #230 - Implemented natural functions keys rule - Recognition is based on Enter/PFX with text beneath it (exactly beneath). - Added test - Re-factored ScreenActionFact to support separated action/caption

--- openlegacy-designtime/src/main/java/org/openlegacy/designtime/terminal/analyzer/support/fact_processors/AbstractActionFactProcessor.java
+++ openlegacy-designtime/src/main/java/org/openlegacy/designtime/terminal/analyzer/support/fact_processors/AbstractActionFactProcessor.java
@@ -1,17 +1,11 @@
 package org.openlegacy.designtime.terminal.analyzer.support.fact_processors;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.openlegacy.designtime.terminal.analyzer.ScreenFact;
 import org.openlegacy.designtime.terminal.analyzer.ScreenFactProcessor;
 import org.openlegacy.designtime.terminal.analyzer.TerminalActionAnalyzer;
 import org.openlegacy.designtime.terminal.model.ScreenEntityDesigntimeDefinition;
 import org.openlegacy.terminal.TerminalPosition;
 import org.openlegacy.terminal.definitions.TerminalActionDefinition;
-
-import java.text.MessageFormat;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.inject.Inject;
 
@@ -20,8 +14,6 @@ public abstract class AbstractActionFactProcessor implements ScreenFactProcessor
 	@Inject
 	private TerminalActionAnalyzer terminalActionAnalyzer;
 
-	private final static Log logger = LogFactory.getLog(AbstractActionFactProcessor.class);
-
 	public boolean accept(ScreenFact screenFact) {
 		return screenFact.getClass() == ScreenActionFact.class;
 	}
@@ -29,20 +21,12 @@ public abstract class AbstractActionFactProcessor implements ScreenFactProcessor
 	public void process(ScreenEntityDesigntimeDefinition screenEntityDefinition, ScreenFact screenFact) {
 		ScreenActionFact screenActionFact = (ScreenActionFact)screenFact;
 
-		Pattern pattern = Pattern.compile(screenActionFact.getRegex());
-		Matcher match = pattern.matcher(screenActionFact.getCaptionAction());
+		TerminalActionDefinition actionDefinition = buildActionDefinition(screenActionFact.getAction(),
+				screenActionFact.getCaption(), screenActionFact.getTerminalPosition());
 
-		match.find();
-		if (match.groupCount() < 2) {
-			logger.warn(MessageFormat.format("text is not in the format of: action -> displayName",
-					screenActionFact.getCaptionAction(), screenEntityDefinition.getEntityName()));
-			return;
+		if (actionDefinition != null) {
+			addAction(screenEntityDefinition, screenActionFact, actionDefinition);
 		}
-
-		TerminalActionDefinition actionDefinition = buildActionDefinition(match.group(1), match.group(2),
-				screenActionFact.getTerminalPosition());
-
-		addAction(screenEntityDefinition, screenActionFact, actionDefinition);
 
 	}
 
