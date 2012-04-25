0606893db53a4617b34bc8dc474b417e6696efc0
Open legacy <openlegacy2@gmail.com>
2012-04-25 10:53:42 +0300

    refers #227 - initial work on extended demo - Added notes sticky notes tab to item details - not updated yet - Added images tab - still static - Added graphical panels - Added rich text editor - not updated yet - Modified the trail to be toys inventory

--- openlegacy/openlegacy-templates/openlegacy-mvc-sample/src/main/java/org/openlegacy/terminal/samples/mvc/controllers/WorkWithItemMaster1CompositeController.java
+++ openlegacy/openlegacy-templates/openlegacy-mvc-sample/src/main/java/org/openlegacy/terminal/samples/mvc/controllers/WorkWithItemMaster1CompositeController.java
@@ -1,9 +1,10 @@
 package org.openlegacy.terminal.samples.mvc.controllers;
 
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for screen WorkWithItemMaster1Composite
@@ -13,8 +14,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class WorkWithItemMaster1CompositeController {
 
 	// handle page initial display
-    @RequestMapping(method = RequestMethod.GET)
-    public String show(Model uiModel) {
-        return "WorkWithItemMaster1Composite";
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	public String show(Model uiModel) {
+		return "WorkWithItemMaster1Composite";
+	}
+
+	@RequestMapping(value = "/updateNote", method = RequestMethod.GET)
+	public @ResponseBody
+	String updateNode() {
+		System.out.println("On the server");
+		return "";
+	}
+
 }
