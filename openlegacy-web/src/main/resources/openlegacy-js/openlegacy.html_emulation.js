b842aaa0bd5415723e103e8d7d369025f3c48dbf
Open legacy <openlegacy2@gmail.com>
2012-06-18 22:50:57 +0300

    fixes #223 - moved JS files into OL web JAR. Removed duplication - Fixed cursor location problem

--- openlegacy-web/src/main/resources/openlegacy-js/openlegacy.html_emulation.js
+++ openlegacy-web/src/main/resources/openlegacy-js/openlegacy.html_emulation.js
@@ -14,6 +14,12 @@ var terminalSession = new TerminalSession();
 
 dojo.addOnLoad(function(){
     var elements = getMainForm().elements;
+    var focusFieldName = getMainForm().TerminalCursor.value;
+    
+    var focusField = dojo.byId(focusFieldName);
+    if (focusField != null){
+    	if (focusField.focus != null) focusField.focus();
+    }
     for (var i=0;i<elements.length;i++){
     	var element = elements[i];
     	dojo.connect(element, "onfocus", function(e) {
