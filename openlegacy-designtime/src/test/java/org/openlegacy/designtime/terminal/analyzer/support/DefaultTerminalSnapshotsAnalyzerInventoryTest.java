8cc39dbb69971b0b2d2d8a607b6c6e15ebe3810d
Open legacy <openlegacy2@gmail.com>
2012-07-08 16:40:11 +0300

    *** added test for rules performance

--- openlegacy-designtime/src/test/java/org/openlegacy/designtime/terminal/analyzer/support/DefaultTerminalSnapshotsAnalyzerInventoryTest.java
+++ openlegacy-designtime/src/test/java/org/openlegacy/designtime/terminal/analyzer/support/DefaultTerminalSnapshotsAnalyzerInventoryTest.java
@@ -39,4 +39,10 @@ public class DefaultTerminalSnapshotsAnalyzerInventoryTest extends AbstractAnaly
 
 	}
 
+	@Test
+	public void testInventoryAppTrail() throws TemplateException, IOException {
+		// TODO Take ~35 seconds - need to improve rules execution time
+		snapshotsAnalyzer.analyzeTrail(getClass().getResourceAsStream("inventory_session.trail.xml"));
+	}
+
 }
