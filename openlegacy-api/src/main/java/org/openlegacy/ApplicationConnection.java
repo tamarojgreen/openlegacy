ce678a2feb27bfa3cbdb84f1c714d84e27be3215
roi0507 <roi0507@gmail.com>
2012-07-19 23:40:07 +0300

    *** workaround for build fail - older web-resource JAR version

---
+++ openlegacy-api/src/main/java/org/openlegacy/ApplicationConnection.java
@@ -0,0 +1,31 @@
+package org.openlegacy;
+
+/**
+ * Represent an connection to an abstract application. Currently implemented for
+ * {@link org.openlegacy.terminal.TerminalConnection}.
+ * 
+ * Provides API for retrieving a snapshot with/without cached, and API for common abstract application interaction
+ * 
+ * @author Roi Mor
+ * 
+ * @param <S>
+ *            The type of snapshot used
+ * @param <A>
+ *            The type of send action used
+ */
+public interface ApplicationConnection<S extends Snapshot, A extends SendAction> {
+
+	S getSnapshot();
+
+	S fetchSnapshot();
+
+	Object getDelegate();
+
+	String getSessionId();
+
+	boolean isConnected();
+
+	void disconnect();
+
+	void doAction(A sendAction);
+}
