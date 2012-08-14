0ab69e55bac8829009626d6281ebc37aad86ad27
roi0507 <roi0507@gmail.com>
2012-08-14 22:44:43 +0300

    refers #308 - initial designtime context for RTL applications. - 2 RTL rules - Tests - Sprint RTL designtime context

--- openlegacy-designtime/src/test/java/org/openlegacy/designtime/terminal/analyzer/support/DefaultTerminalSnapshotsAnalyzerHebrewTest.java
+++ openlegacy-designtime/src/test/java/org/openlegacy/designtime/terminal/analyzer/support/DefaultTerminalSnapshotsAnalyzerHebrewTest.java
@@ -6,18 +6,35 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openlegacy.terminal.TerminalSnapshot;
 import org.openlegacy.terminal.definitions.ScreenEntityDefinition;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
-@Ignore
 @ContextConfiguration("DefaultTerminalSnapshotsAnalyzerHebrewTest-context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class DefaultTerminalSnapshotsAnalyzerHebrewTest extends AbstractAnalyzerTest {
 
+	@Test
+	public void testBidiScreens() throws TemplateException, IOException {
+		//
+		List<TerminalSnapshot> snapshots = snapshotsLoader.loadSnapshots(
+				getClass().getResource("/org/openlegacy/designtime/terminal/analyzer/support/hebrew").getFile(),
+				"BidiFormScreen.xml");
+
+		snapshotsOrganizer.setMatchingPercent(95);
+		Map<String, ScreenEntityDefinition> definitions = snapshotsAnalyzer.analyzeSnapshots(snapshots);
+
+		Assert.assertEquals(1, definitions.size());
+		assertScreenContent(definitions.get("MscBabrit"), "hebrew/HebrewScreen.java.expected");
+
+	}
+
+	@Ignore
 	@Test
 	public void testHebrewAS400AppGenerate() throws TemplateException, IOException {
 		snapshotsOrganizer.setMatchingPercent(95);
