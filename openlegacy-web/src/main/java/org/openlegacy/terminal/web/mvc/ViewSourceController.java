eb3bd1863f028eab7014abc1c1f836a7ca2130f5
roi0507 <roi0507@gmail.com>
2012-08-21 21:25:08 +0300

    fixes #304 - Added view source controller (disabled by default, enable in the demo) + links in demo (Java files works only from WAR)

---
+++ openlegacy-web/src/main/java/org/openlegacy/terminal/web/mvc/ViewSourceController.java
@@ -0,0 +1,51 @@
+package org.openlegacy.terminal.web.mvc;
+
+import org.apache.commons.io.IOUtils;
+import org.openlegacy.exceptions.OpenLegacyRuntimeException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.HtmlUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping(value = "/viewSource")
+public class ViewSourceController {
+
+	private boolean enabled = false;
+	private String htmlOutput = "<html><body><pre>{0}</pre></body></html>";
+
+	@RequestMapping(method = RequestMethod.GET)
+	public void viewSource(@RequestParam("file") String file, HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		if (!enabled) {
+			throw (new OpenLegacyRuntimeException("View source controller is not enabled"));
+		}
+
+		ServletContext servletContext = request.getSession().getServletContext();
+
+		InputStream resource = servletContext.getResourceAsStream(file);
+		if (resource == null) {
+			throw (new OpenLegacyRuntimeException("Specified resource doesn''t exists"));
+		}
+		String content = IOUtils.toString(resource);
+		content = HtmlUtils.htmlEscape(content);
+		response.getWriter().write(MessageFormat.format(htmlOutput, content));
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public void setHtmlOutput(String htmlOutput) {
+		this.htmlOutput = htmlOutput;
+	}
+}
