1654bea2666a3a2197b3f9459e610fa16b2d2d8a
roi0507 <roi0507@gmail.com>
2012-09-17 22:40:38 +0300

    fixes #332 - Added JUnit for sample MVC - Wrote a new test for demo - copy tests into WAR - added link on left menu - fixed applicationContext-test.xml. From some reason it scans controllers, and fails due to missing webcomponents context. - Renamed incorrect test source folder

---
+++ openlegacy/openlegacy-templates/openlegacy-mvc-sample/src/test/java/org/openlegacy/terminal/samples/model/MvcSampleTest.java
@@ -0,0 +1,40 @@
+package org.openlegacy.terminal.samples.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openlegacy.exceptions.RegistryException;
+import org.openlegacy.modules.login.Login;
+import org.openlegacy.modules.login.LoginException;
+import org.openlegacy.terminal.TerminalSession;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+
+import junit.framework.Assert;
+
+@ContextConfiguration("/META-INF/spring/applicationContext-test.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class MvcSampleTest {
+
+	@Inject
+	private ApplicationContext applicationContext;
+
+	@Test
+	public void testSession() throws RegistryException, LoginException {
+		TerminalSession terminalSession = applicationContext.getBean(TerminalSession.class);
+		terminalSession.getModule(Login.class).login("user", "pwd");
+
+		Items items = terminalSession.getEntity(Items.class);
+		Assert.assertEquals(12, items.getItemsRecords().size());
+
+		ItemDetails itemDetails = terminalSession.getEntity(ItemDetails.class, 2000);
+		Assert.assertEquals(2000, itemDetails.getItemNumber().intValue());
+
+		WarehouseDetails warehouseDetails = terminalSession.getEntity(WarehouseDetails.class, 3);
+		Assert.assertEquals(3, warehouseDetails.getWarehouseNumber().intValue());
+		Assert.assertEquals(1295733600000L, warehouseDetails.getAmendedDate().getTime());
+
+	}
+}
