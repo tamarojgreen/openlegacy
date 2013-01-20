e90136c3121bb6fbc5e74a5d143b5f0b77ec356e
roi <roi@ol-roi>
2013-01-20 20:27:21 +0200

    refs #163 - fix test broke due to mapping change

--- openlegacy-designtime/src/test/java/org/openlegacy/designtime/terminal/generators/TrailJunitGeneratorTest.java
+++ openlegacy-designtime/src/test/java/org/openlegacy/designtime/terminal/generators/TrailJunitGeneratorTest.java
@@ -4,7 +4,6 @@ import freemarker.template.TemplateException;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openlegacy.designtime.terminal.analyzer.support.DefaultTerminalSnapshotsAnalyzer;
@@ -42,7 +41,6 @@ public class TrailJunitGeneratorTest {
 		assertGenerateJunit("test.trail", "TrailJunit.java.expected");
 	}
 
-	@Ignore("Need to reconfigure keyboard mapping")
 	@Test
 	public void testDemoSessionJunit() throws Exception {
 		assertGenerateJunit("demo_session.trail", "TrailJunitDemoSession.java.expected");
