f560ca60ddd0f453693b242dff1492b58abca29e
roi0507 <roi0507@gmail.com>
2013-04-14 19:57:31 +0300

    *** remove delete test

--- openlegacy-impl/src/test/java/org/openlegacy/recognizers/RecognizersSuite.java
+++ openlegacy-impl/src/test/java/org/openlegacy/recognizers/RecognizersSuite.java
@@ -3,13 +3,10 @@ package org.openlegacy.recognizers;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import org.openlegacy.recognizers.composite.CompositeScreensRecognizerTest;
 import org.openlegacy.recognizers.pattern.PatternBasedScreensRecognizerTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ RegistryBasedScreensRecognizerTest.class,
-		PatternBasedScreensRecognizerTest.class,
-		CompositeScreensRecognizerTest.class })
+@SuiteClasses({ RegistryBasedScreensRecognizerTest.class, PatternBasedScreensRecognizerTest.class })
 public class RecognizersSuite {
 
 }
