5f03686b040caa56dd7dc3a7b3777ce6a21f8e1b
roi0507 <roi0507@gmail.com>
2013-08-08 15:09:46 +0300

    fixes #295 - avoid bidi conversion when no bidi chars exists.

--- openlegacy-utils/src/main/java/org/openlegacy/utils/BidiUtil.java
+++ openlegacy-utils/src/main/java/org/openlegacy/utils/BidiUtil.java
@@ -35,6 +35,9 @@ public class BidiUtil {
 	}
 
 	public static String convertToVisual(String value) {
+		if (!containsRTLChar(value)) {
+			// return value;
+		}
 		Bidi bidi = new Bidi();
 		bidi.setPara(value, Bidi.RTL, null);
 		String newValue = bidi.writeReordered(Bidi.DO_MIRRORING);
@@ -44,4 +47,68 @@ public class BidiUtil {
 		value = newValue;
 		return value;
 	}
+
+	public static boolean containsRTLChar(String str) {
+		return containsArabicChar(str) || containsHebrewChar(str);
+	}
+
+	public static boolean containsHebrewChar(String str) {
+		boolean reply = false;
+		for (int i = 0; i < str.length(); i++) {
+			if (isHebrewChar(str.charAt(i))) {
+				reply = true;
+				break;
+			}
+		}
+		return reply;
+	}
+
+	public static boolean containsArabicChar(String str) {
+		boolean reply = false;
+		for (int i = 0; i < str.length(); i++) {
+			if (isArabicChar(str.charAt(i))) {
+				reply = true;
+				break;
+			}
+		}
+		return reply;
+	}
+
+	public static boolean isHebrewChar(char inputChar) {
+		if (inputChar >= 0x5D0 && inputChar <= 0x5EA) { // HEBREW ALEF TO TAV
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean isArabicChar(char inputChar) {
+		if ((inputChar >= 0x0600 && inputChar <= 0x06FF) // 0600-06FF Arabic
+				|| (inputChar >= 0xFB50 && inputChar <= 0xFDFD) // FB50-FDFD Unicode Characters in the Arabic Presentation Forms-A
+																// Block
+				|| (isArabicCharFormB(inputChar)) // FE70-FEFC Unicode Characters in the Arabic Presentation Forms-B Block
+		) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean isArabicCharFormB(char inputChar) {
+		if (inputChar >= 0xFE70 && inputChar <= 0xFEFC) // FE70-FEFC Unicode Characters in the Arabic Presentation Forms-B Block
+		{
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean isArabicNumber(char inputChar) {
+		if (inputChar >= 0x0660 && inputChar <= 0x0669) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 }
