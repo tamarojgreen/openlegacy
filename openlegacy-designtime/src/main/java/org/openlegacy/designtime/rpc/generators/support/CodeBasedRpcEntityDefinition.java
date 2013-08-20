bc90d568a7add6371618b2e5689c100527a048ed
roi0507 <grinholz2>
2013-08-20 20:55:02 +0300

    refs #296 - add recursive key support for RPC code based entities

--- openlegacy-designtime/src/main/java/org/openlegacy/designtime/rpc/generators/support/CodeBasedRpcEntityDefinition.java
+++ openlegacy-designtime/src/main/java/org/openlegacy/designtime/rpc/generators/support/CodeBasedRpcEntityDefinition.java
@@ -3,13 +3,17 @@ package org.openlegacy.designtime.rpc.generators.support;
 import org.openlegacy.EntityDefinition;
 import org.openlegacy.annotations.rpc.Languages;
 import org.openlegacy.definitions.ActionDefinition;
+import org.openlegacy.definitions.FieldDefinition;
+import org.openlegacy.definitions.PartEntityDefinition;
 import org.openlegacy.designtime.rpc.generators.RpcPojoCodeModel;
 import org.openlegacy.designtime.terminal.generators.support.AbstractCodeBasedEntityDefinition;
 import org.openlegacy.rpc.definitions.RpcEntityDefinition;
 import org.openlegacy.rpc.definitions.RpcFieldDefinition;
+import org.openlegacy.rpc.definitions.RpcPartEntityDefinition;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +23,8 @@ public class CodeBasedRpcEntityDefinition extends AbstractCodeBasedEntityDefinit
 
 	private List<ActionDefinition> actions;
 	private Map<String, RpcFieldDefinition> fields;
+
+	private List<RpcFieldDefinition> keyFields;
 
 	public CodeBasedRpcEntityDefinition(RpcPojoCodeModel codeModel, File packageDir) {
 		super(codeModel, packageDir);
@@ -60,5 +66,40 @@ public class CodeBasedRpcEntityDefinition extends AbstractCodeBasedEntityDefinit
 	 */
 	public Languages getLanguage() {
 		return getCodeModel().getLanguage();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<? extends FieldDefinition> getKeys() {
+		if (keyFields != null) {
+			return keyFields;
+		}
+
+		keyFields = new ArrayList<RpcFieldDefinition>();
+
+		keyFields.addAll((Collection<? extends RpcFieldDefinition>)super.getKeys());
+
+		Collection<PartEntityDefinition<RpcFieldDefinition>> parts = getPartsDefinitions().values();
+		for (PartEntityDefinition<RpcFieldDefinition> part : parts) {
+			RpcPartEntityDefinition rpcPart = (RpcPartEntityDefinition)part;
+			addKeysFromParts(rpcPart.getInnerPartsDefinitions().values(), keyFields);
+		}
+		return keyFields;
+	}
+
+	private static void addKeysFromParts(Collection<RpcPartEntityDefinition> parts, List<RpcFieldDefinition> keyFields) {
+		for (PartEntityDefinition<RpcFieldDefinition> part : parts) {
+			RpcPartEntityDefinition rpcPart = (RpcPartEntityDefinition)part;
+			for (RpcFieldDefinition rpcField : rpcPart.getFieldsDefinitions().values()) {
+				if (rpcField.isKey()) {
+					keyFields.add(rpcField);
+				}
+			}
+			for (PartEntityDefinition<RpcFieldDefinition> innerPart : rpcPart.getInnerPartsDefinitions().values()) {
+				RpcPartEntityDefinition rpcInnerPart = (RpcPartEntityDefinition)innerPart;
+				addKeysFromParts(rpcInnerPart.getInnerPartsDefinitions().values(), keyFields);
+			}
+		}
+
 	}
 }
