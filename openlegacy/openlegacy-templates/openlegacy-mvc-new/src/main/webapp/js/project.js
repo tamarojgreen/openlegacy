a88d2d2d0942742f4e386abefc900fee7c17eba0
roi0507 <grinholz2>
2013-08-22 21:34:50 +0300

    ** Added busy icon and optional emulation in Ajax

--- openlegacy/openlegacy-templates/openlegacy-mvc-new/src/main/webapp/js/project.js
+++ openlegacy/openlegacy-templates/openlegacy-mvc-new/src/main/webapp/js/project.js
@@ -12,3 +12,15 @@ function openMobileLookupDialog(fieldId,displayFieldId,url,style){
 	    });
 	});	
 }
+
+
+/* Uncomment for Ajax emulation
+function doAction(keyboardKey){
+	getMainForm().KeyboardKey.value = keyboardKey;
+	doAjaxPost(getMainForm().name,"container","","body",emulationOnLoad);
+}
+
+if (window.terminalSession){
+	terminalSession.doAction = doAction; 
+}
+*/
