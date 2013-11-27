92c8c6a262d1c08d9b1e6f4bef7604cb03dc2f3b
roi0507 <roi0507@gmail.com>
2013-11-27 16:59:15 +0200

    fixes #354 - add login redirect when screen has params

--- openlegacy-web/src/main/java/org/openlegacy/terminal/mvc/web/interceptors/LoginInterceptor.java
+++ openlegacy-web/src/main/java/org/openlegacy/terminal/mvc/web/interceptors/LoginInterceptor.java
@@ -5,6 +5,8 @@ import org.openlegacy.terminal.TerminalSession;
 import org.openlegacy.terminal.modules.login.LoginMetadata;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import java.net.URLEncoder;
+
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,12 +21,12 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		String requestUri = request.getRequestURI().toLowerCase().substring(1);
-		if (!request.getRequestURI().contains(".") && !requestUri.endsWith(MvcConstants.LOGIN_VIEW)
+		String requestUri = request.getRequestURI().substring(1);
+		if (!request.getRequestURI().contains(".") && !requestUri.toLowerCase().endsWith(MvcConstants.LOGIN_VIEW)
 				&& !requestUri.endsWith("logoff") && loginMetadata.getLoginScreenDefinition() != null
 				&& !terminalSession.isConnected()) {
 			String requestedPage = requestUri.substring(requestUri.indexOf("/") + 1);
-			response.sendRedirect("login?requestedUrl=" + requestedPage);
+			response.sendRedirect(request.getContextPath() + "/login?requestedUrl=" + URLEncoder.encode(requestedPage, "UTF-8"));
 			return false;
 		}
 		return true;
