6c902dcc44219d27c076d826df2579f432b07a84
roi0507 <roi0507@gmail.com>
2013-12-30 15:53:38 +0200

    fixes #396 - preserve URL params

--- openlegacy-web/src/main/java/org/openlegacy/terminal/mvc/web/interceptors/LoginInterceptor.java
+++ openlegacy-web/src/main/java/org/openlegacy/terminal/mvc/web/interceptors/LoginInterceptor.java
@@ -6,6 +6,8 @@ import org.openlegacy.terminal.modules.login.LoginMetadata;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import java.net.URLEncoder;
+import java.text.MessageFormat;
+import java.util.Set;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
@@ -26,6 +28,12 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 				&& !requestUri.endsWith("logoff") && loginMetadata.getLoginScreenDefinition() != null
 				&& !terminalSession.isConnected()) {
 			String requestedPage = requestUri.substring(requestUri.indexOf("/") + 1);
+			@SuppressWarnings("unchecked")
+			Set<String> paramNames = request.getParameterMap().keySet();
+			for (String paramName : paramNames) {
+				requestedPage = MessageFormat.format("{0}&{1}={2}", requestedPage, paramName, request.getParameter(paramName));
+			}
+			requestedPage = requestedPage.replaceFirst("&", "?");
 			response.sendRedirect(request.getContextPath() + "/login?requestedUrl=" + URLEncoder.encode(requestedPage, "UTF-8"));
 			return false;
 		}
