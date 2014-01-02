38dd6eec861bbecfe13d8f27a6dd7766e0d82c74
roi0507 <roi0507@gmail.com>
2014-01-02 08:05:59 +0200

    refs #400 - fix test

--- openlegacy-impl/src/test/java/org/openlegacy/terminal/support/mock/WindowScreen1.java
+++ openlegacy-impl/src/test/java/org/openlegacy/terminal/support/mock/WindowScreen1.java
@@ -3,9 +3,23 @@ package org.openlegacy.terminal.support.mock;
 import org.openlegacy.annotations.screen.Identifier;
 import org.openlegacy.annotations.screen.ScreenEntity;
 import org.openlegacy.annotations.screen.ScreenIdentifiers;
+import org.openlegacy.terminal.definitions.TerminalActionDefinition;
+
+import java.util.Collections;
+import java.util.List;
 
 @ScreenEntity(window = true)
 @ScreenIdentifiers(identifiers = { @Identifier(row = 6, column = 40, value = "Win title") })
-public class WindowScreen1 {
+public class WindowScreen1 implements org.openlegacy.terminal.ScreenEntity {
+
+	public String getFocusField() {
+		return null;
+	}
+
+	public void setFocusField(String focusField) {}
+
+	public List<TerminalActionDefinition> getActions() {
+		return Collections.emptyList();
+	}
 
 }
