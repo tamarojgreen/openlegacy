be058916da7090fea176f9623373e54602d320e8
roi0507 <roi0507@gmail.com>
2014-01-12 16:23:19 +0200

    fixes #413 - enable numpad + as field plus

--- openlegacy-web/src/main/resources/openlegacy-js/openlegacy.keyboard.js
+++ openlegacy-web/src/main/resources/openlegacy-js/openlegacy.keyboard.js
@@ -8,44 +8,64 @@ function sendFunctionKey(keyCode,shiftKey){
 
 require(["dojo/ready"], function(ready){
 	ready(function(){
-		require(["dojo/on", "dojo/_base/window", "dojo/keys"], function(on, win, keys){
-			on(win.doc, "keydown", function(e){
-				var handled = true;
-				switch (e.keyCode) {
-					case keys.ESCAPE:
-						terminalSession.doAction("ESC");
-						break;
-					case keys.ENTER:
-						terminalSession.doAction("ENTER");
-						break;
-					case keys.F1:
-					case keys.F2:
-					case keys.F3:
-					case keys.F4:
-					case keys.F5:
-					case keys.F6:
-					case keys.F7:
-					case keys.F8:
-					case keys.F9:
-					case keys.F10:
-					case keys.F11:
-					case keys.F12:
-						sendFunctionKey(e.keyCode,e.shiftKey);
-						break;
-					case keys.PAGE_UP:
-						terminalSession.doAction("PAGEUP");
-						break;
-					case keys.PAGE_DOWN:
-						terminalSession.doAction("PAGEDOWN");
-						break;
-					default:
-						handled = false;
-				}
-				if (handled){
-					dojo.stopEvent(e);
-				}
-				return !handled;
-			});
-		});
+		mapEmulationKeyboard();
 	});
 });
+
+function mapEmulationKeyboard(){
+	require(["dojo/on", "dojo/_base/window", "dojo/keys"], function(on, win, keys){
+		on(win.doc, "keydown", function(e){
+			var handled = true;
+			switch (e.keyCode) {
+				case keys.ESCAPE:
+					terminalSession.doAction("ESC");
+					break;
+				case keys.ENTER:
+					terminalSession.doAction("ENTER");
+					break;
+				case keys.F1:
+				case keys.F2:
+				case keys.F3:
+				case keys.F4:
+				case keys.F5:
+				case keys.F6:
+				case keys.F7:
+				case keys.F8:
+				case keys.F9:
+				case keys.F10:
+				case keys.F11:
+				case keys.F12:
+					sendFunctionKey(e.keyCode,e.shiftKey);
+					break;
+				case keys.PAGE_UP:
+					terminalSession.doAction("PAGEUP");
+					break;
+				case keys.PAGE_DOWN:
+					terminalSession.doAction("PAGEDOWN");
+					break;
+				case 107:  // Numpad +
+					fieldPlus(e.target);
+					break;
+				default:
+					handled = false;
+			}
+			if (handled){
+				dojo.stopEvent(e);
+			}
+			return !handled;
+		});
+	});
+}
+
+function fieldPlus(element){
+
+	var inputs = document.getElementsByTagName("input");
+	for (var i=0;i<inputs.length;i++){
+		if (inputs[i].tabIndex != null && element.tabIndex != null){
+			if (inputs[i].tabIndex == (element.tabIndex+1)){
+				inputs[i].focus();
+				break;
+			}
+		}
+	}
+}
