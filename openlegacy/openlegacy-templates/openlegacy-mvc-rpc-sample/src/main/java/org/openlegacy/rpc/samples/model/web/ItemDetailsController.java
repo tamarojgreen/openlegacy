a7dd75a8b8b26dfb2a87b801c93b8f56e19df7c8
dganitd <dganit.david@gmail.com>
2014-01-29 13:01:49 +0200

    add help to RPC MVC Demo

--- openlegacy/openlegacy-templates/openlegacy-mvc-rpc-sample/src/main/java/org/openlegacy/rpc/samples/model/web/ItemDetailsController.java
+++ openlegacy/openlegacy-templates/openlegacy-mvc-rpc-sample/src/main/java/org/openlegacy/rpc/samples/model/web/ItemDetailsController.java
@@ -1,5 +1,6 @@
 package org.openlegacy.rpc.samples.model.web;
 
+import org.apache.commons.io.IOUtils;
 import org.openlegacy.rpc.RpcActions;
 import org.openlegacy.rpc.RpcSession;
 import org.openlegacy.rpc.samples.model.ItemDetails;
@@ -8,8 +9,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.net.URL;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/ItemDetails")
@@ -18,10 +24,16 @@ public class ItemDetailsController {
 	@Inject
 	private RpcSession rpcSession;
 
+	@RequestMapping(value = "/help")
+	public @ResponseBody
+	String help(HttpServletRequest request) throws IOException {
+		URL resource = request.getSession().getServletContext().getResource("/help/ItemDetails.html");
+		return IOUtils.toString(resource.openStream());
+	}
+
 	@RequestMapping(value = "/{key:[\\w+[-_ ]*\\w+]+}", method = RequestMethod.GET)
 	public String show(Model uiModel, @PathVariable("key") String key) {
-		if (key.equals("help"))
-			return "help";
+
 		ItemDetails itemDetails = rpcSession.getEntity(ItemDetails.class, key);
 
 		uiModel.addAttribute("itemDetails", itemDetails);
--- openlegacy/openlegacy-templates/openlegacy-mvc-rpc-sample/src/main/java/org/openlegacy/rpc/samples/model/web/ItemsController.java
+++ openlegacy/openlegacy-templates/openlegacy-mvc-rpc-sample/src/main/java/org/openlegacy/rpc/samples/model/web/ItemsController.java
@@ -1,13 +1,19 @@
 package org.openlegacy.rpc.samples.model.web;
 
+import org.apache.commons.io.IOUtils;
 import org.openlegacy.rpc.RpcActions;
 import org.openlegacy.rpc.RpcSession;
 import org.openlegacy.rpc.samples.model.Items;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.net.URL;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/Items")
@@ -16,6 +22,13 @@ public class ItemsController {
 	@Inject
 	private RpcSession rpcSession;
 
+	@RequestMapping(value = "/help")
+	public @ResponseBody
+	String help(HttpServletRequest request) throws IOException {
+		URL resource = request.getSession().getServletContext().getResource("/help/Items.html");
+		return IOUtils.toString(resource.openStream());
+	}
+	
 	@RequestMapping
 	public String show(Model uiModel) {
 		Items items = new Items();
