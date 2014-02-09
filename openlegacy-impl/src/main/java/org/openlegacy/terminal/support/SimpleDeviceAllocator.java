79c990149b3f4452fa15327561b714819d6cf624
roi0507 <roi0507@gmail.com>
2014-02-09 17:05:40 +0200

    *** Dummy device allocator for testings

---
+++ openlegacy-impl/src/main/java/org/openlegacy/terminal/support/SimpleDeviceAllocator.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2014 OpenLegacy Inc.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     OpenLegacy Inc. - initial API and implementation
+ *******************************************************************************/
+package org.openlegacy.terminal.support;
+
+import org.openlegacy.terminal.DeviceAllocator;
+
+public class SimpleDeviceAllocator implements DeviceAllocator {
+
+	private String deviceName;
+
+	public String allocate(String pullName) {
+		return deviceName;
+	}
+
+	public void setDeviceName(String deviceName) {
+		this.deviceName = deviceName;
+	}
+}
