00af1b423dec0648ae937fdf82d19ec0707860a2
roi0507 <roi0507@gmail.com>
2014-02-11 10:23:12 +0200

    refs #415 - remove dependency in AS/400 fetcher within designtime. User should edit designtime context to enable it

---
+++ openlegacy-designtime/src/main/java/org/openlegacy/designtime/rpc/MockSourceFetcher.java
@@ -0,0 +1,16 @@
+package org.openlegacy.designtime.rpc;
+
+import org.openlegacy.exceptions.OpenLegacyException;
+import org.openlegacy.rpc.SourceFetcher;
+
+public class MockSourceFetcher implements SourceFetcher {
+
+	public byte[] fetch(String host, String user, String password, String legacyFile) throws OpenLegacyException {
+		return new byte[0];
+	}
+
+	public String convertExtension(String legacyName) {
+		return legacyName;
+	}
+
+}
