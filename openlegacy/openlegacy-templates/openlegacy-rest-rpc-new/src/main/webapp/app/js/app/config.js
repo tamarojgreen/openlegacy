7154402a48755d32fed383c0ed0431935ffce937
roi0507 <roi0507@gmail.com>
2014-09-14 22:22:58 +0300

    refs #600 - edit config JS for rest-rpc-new

--- openlegacy/openlegacy-templates/openlegacy-rest-rpc-new/src/main/webapp/app/js/app/config.js
+++ openlegacy/openlegacy-templates/openlegacy-rest-rpc-new/src/main/webapp/app/js/app/config.js
@@ -1,1 +1,1 @@
-var olConfig = {baseUrl:"/openlegacy-rest-new/",hostUrl:"http://host.name/application.name"};
+var olConfig = {baseUrl:"/openlegacy-rest-rpc-new/",hostUrl:"http://host.name/application.name"};
