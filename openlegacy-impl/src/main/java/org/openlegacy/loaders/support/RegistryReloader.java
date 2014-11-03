51f8fe0c0f4e770e3ac7fbb6d643e87c84fba36b
roi0507 <roi0507@gmail.com>
2014-11-03 06:48:54 +0200

    refs #738 - Remove JRebel runtime plugin - Added a registry reloader servlet which loads modified classes. Works great when JVM is DCEVM

---
+++ openlegacy-impl/src/main/java/org/openlegacy/loaders/support/RegistryReloader.java
@@ -0,0 +1,100 @@
+package org.openlegacy.loaders.support;
+
+import org.openlegacy.EntitiesRegistry;
+import org.openlegacy.loaders.RegistryLoader;
+import org.openlegacy.utils.FileUtils;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Utility class for designtime to support loading modified classes
+ * 
+ * @author Roi Mor
+ * 
+ */
+public class RegistryReloader {
+
+	private Map<File, Long> packageModified = new HashMap<File, Long>();
+	private String apiSourcesPath = "src/main/java";;
+
+	private long compileInterval = 3000;
+
+	public RegistryReloader(String apiSourcesPath, long compileInterval) {
+		this.apiSourcesPath = apiSourcesPath;
+		this.compileInterval = compileInterval;
+	}
+
+	public void scanForChanges(final File webApp, final EntitiesRegistry<?, ?, ?> registry, final RegistryLoader registryLoader) {
+		File projectRoot = webApp.getParentFile().getParentFile().getParentFile();
+
+		File sourcesDir = new File(projectRoot, apiSourcesPath);
+		List<String> packages = registry.getPackages();
+		for (String package1 : packages) {
+			String packageRelativeDir = package1.replaceAll("\\.", "/");
+			File packageSourceDir = new File(sourcesDir, packageRelativeDir);
+			Long packageLastModified = packageModified.get(packageSourceDir);
+			// find all classes
+			if (packageLastModified != null) {
+				List<File> javaSources = getSourcesByLastModified(packageSourceDir);
+
+				long fileLastModified = packageLastModified;
+				for (File file : javaSources) {
+					if (packageLastModified < file.lastModified()) {
+						try {
+							Thread.sleep(compileInterval);
+							Class<?> clazz = Class.forName(package1 + "." + FileUtils.fileWithoutAnyExtension(file.getName()));
+							registryLoader.loadSingleClass(registry, clazz, true);
+							fileLastModified = file.lastModified();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					} else {
+						break;
+					}
+				}
+				if (fileLastModified > packageLastModified) {
+					packageModified.put(packageSourceDir, fileLastModified);
+				}
+			} else {
+				List<File> javaSources = getSourcesByLastModified(packageSourceDir);
+				long fileModified = packageSourceDir.lastModified();
+				if (javaSources.size() > 0) {
+					fileModified = javaSources.get(0).lastModified();
+				}
+				packageModified.put(packageSourceDir, fileModified);
+			}
+		}
+	}
+
+	private static List<File> getSourcesByLastModified(File sourcesDir) {
+		File[] files = sourcesDir.listFiles(new FileFilter() {
+
+			public boolean accept(File file) {
+				return file.getName().endsWith(".java");
+			}
+		});
+		List<File> javaSources = Arrays.asList(files);
+		Collections.sort(javaSources, new Comparator<File>() {
+
+			public int compare(File o1, File o2) {
+				return o1.lastModified() > o2.lastModified() ? -1 : 1;
+			}
+		});
+		return javaSources;
+	};
+
+	public void setCompileInterval(long compileInterval) {
+		this.compileInterval = compileInterval;
+	}
+
+	public void setSourcesPath(String sourcesPath) {
+		this.apiSourcesPath = sourcesPath;
+	}
+}
---
+++ openlegacy-web/src/main/java/org/openlegacy/web/RegistryReloaderServlet.java
@@ -0,0 +1,73 @@
+package org.openlegacy.web;
+
+import org.openlegacy.EntitiesRegistry;
+import org.openlegacy.OpenLegacyProperties;
+import org.openlegacy.loaders.RegistryLoader;
+import org.openlegacy.loaders.support.RegistryReloader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import java.io.File;
+import java.util.Collection;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class RegistryReloaderServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private boolean cont = true;
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+
+		final long compileInterval = Long.parseLong(config.getInitParameter("compileInterval"));
+		final long reloadAttemptInterval = Long.parseLong(config.getInitParameter("reloadAttemptInterval"));
+		String apiSourcesDir = config.getInitParameter("apiSourcesPath");
+
+		String enabled = System.getProperty(OpenLegacyProperties.DESIGN_TIME);
+		if (enabled == null || enabled.equals("false")) {
+			System.out.println("API classes reloading is not enabled");
+			return;
+		} else {
+			System.out.println("API classes reloading is running");
+		}
+
+		final ServletContext servletContext = config.getServletContext();
+		ApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
+		final Collection<EntitiesRegistry> registries = context.getBeansOfType(EntitiesRegistry.class).values();
+		final RegistryLoader registryLoader = context.getBean(RegistryLoader.class);
+
+		final RegistryReloader registryReloader = new RegistryReloader(apiSourcesDir, compileInterval);
+		Thread runner = new Thread() {
+
+			@Override
+			public void run() {
+
+				while (cont) {
+					File webAppRootDir = new File(servletContext.getRealPath(""));
+					for (EntitiesRegistry registry : registries) {
+						registryReloader.scanForChanges(webAppRootDir, registry, registryLoader);
+					}
+					try {
+						Thread.sleep(reloadAttemptInterval);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+		};
+		runner.start();
+
+	}
+
+	@Override
+	public void destroy() {
+		cont = false;
+	}
+}
