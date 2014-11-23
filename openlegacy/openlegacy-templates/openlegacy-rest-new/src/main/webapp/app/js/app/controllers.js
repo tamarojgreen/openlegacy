78fec809bd5465b385df5b323215902a86df25fe
roi0507 <roi0507@gmail.com>
2014-11-23 09:22:40 +0200

    fixes #790 - select row only in one table

--- openlegacy/openlegacy-templates/openlegacy-rest-new/src/main/webapp/app/js/app/controllers.js
+++ openlegacy/openlegacy-templates/openlegacy-rest-new/src/main/webapp/app/js/app/controllers.js
@@ -118,11 +118,10 @@
 								$scope.baseUrl = olConfig.baseUrl;
 								$rootScope.$broadcast("olApp:breadcrumbs", data.model.paths);
 								
-								$scope.doActionNoTargetEntity = function(rowIndex, columnName, actionValue) {					
+								$scope.doActionNoTargetEntity = function() {					
 									$scope.model.actions=null;
 									<#list entityDefinition.tableDefinitions?keys as key> 
-									$scope.model.${entityDefinition.tableDefinitions[key].tableEntityName}sActions=null;
-									$scope.model.${entityDefinition.tableDefinitions[key].tableEntityName}s[rowIndex][columnName] = actionValue;
+									$scope.model.${entityDefinition.tableDefinitions[key].tableEntityName?uncap_first}sActions=null;
 								    </#list>	
 									
 									$olHttp.post('${entityDefinition.entityName}/', $scope.model, function(data) {
