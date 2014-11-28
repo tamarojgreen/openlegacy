bba9abf867a4d5e5f10066b1c203e5755c873fa6
Aleksey Yeremeyev <aleksey.y@openlegacy.com>
2014-11-28 17:45:31 +0200

    refs #699 - Added icons for key fields.

--- openlegacy-ide-plugins/openlegacy-ide-enterprise-plugin/openlegacy-enterprise-ide-eclipse/src/main/java/com/openlegacy/enterprise/ide/eclipse/editors/pages/providers/screen/PartsMasterBlockLabelProvider.java
+++ openlegacy-ide-plugins/openlegacy-ide-enterprise-plugin/openlegacy-enterprise-ide-eclipse/src/main/java/com/openlegacy/enterprise/ide/eclipse/editors/pages/providers/screen/PartsMasterBlockLabelProvider.java
@@ -31,30 +31,45 @@ public class PartsMasterBlockLabelProvider extends LabelProvider {
 	@Override
 	public Image getImage(Object element) {
 		if (element instanceof ScreenBooleanFieldModel) {
+			boolean isKey = ((ScreenBooleanFieldModel)element).isKey();
 			if (!((ScreenFieldModel)element).getValidationMessages().isEmpty()) {
-				return Activator.getDefault().getImage(Activator.ICON_BOOLEAN_ERR);
+				return (isKey) ? Activator.getDefault().getImage(Activator.ICON_BOOLEAN_KEY_ERR)
+						: Activator.getDefault().getImage(Activator.ICON_BOOLEAN_ERR);
 			}
-			return Activator.getDefault().getImage(Activator.ICON_BOOLEAN);
+			return (isKey) ? Activator.getDefault().getImage(Activator.ICON_BOOLEAN_KEY) : Activator.getDefault().getImage(
+					Activator.ICON_BOOLEAN);
 		} else if (element instanceof ScreenDateFieldModel) {
+			boolean isKey = ((ScreenDateFieldModel)element).isKey();
 			if (!((ScreenFieldModel)element).getValidationMessages().isEmpty()) {
-				return Activator.getDefault().getImage(Activator.ICON_DATE_ERR);
+				return (isKey) ? Activator.getDefault().getImage(Activator.ICON_DATE_KEY_ERR) : Activator.getDefault().getImage(
+						Activator.ICON_DATE_ERR);
 			}
-			return Activator.getDefault().getImage(Activator.ICON_DATE);
+			return (isKey) ? Activator.getDefault().getImage(Activator.ICON_DATE_KEY) : Activator.getDefault().getImage(
+					Activator.ICON_DATE);
 		} else if (element instanceof ScreenEnumFieldModel) {
+			boolean isKey = ((ScreenEnumFieldModel)element).isKey();
 			if (!((ScreenFieldModel)element).getValidationMessages().isEmpty()) {
-				return Activator.getDefault().getImage(Activator.ICON_ENUM_ERR);
+				return (isKey) ? Activator.getDefault().getImage(Activator.ICON_ENUM_KEY_ERR) : Activator.getDefault().getImage(
+						Activator.ICON_ENUM_ERR);
 			}
-			return Activator.getDefault().getImage(Activator.ICON_ENUM);
+			return (isKey) ? Activator.getDefault().getImage(Activator.ICON_ENUM_KEY) : Activator.getDefault().getImage(
+					Activator.ICON_ENUM);
 		} else if (element instanceof ScreenFieldValuesModel) {
+			boolean isKey = ((ScreenFieldValuesModel)element).isKey();
 			if (!((ScreenFieldModel)element).getValidationMessages().isEmpty()) {
-				return Activator.getDefault().getImage(Activator.ICON_VALUES_ERR);
+				return (isKey) ? Activator.getDefault().getImage(Activator.ICON_VALUES_KEY_ERR)
+						: Activator.getDefault().getImage(Activator.ICON_VALUES_ERR);
 			}
-			return Activator.getDefault().getImage(Activator.ICON_VALUES);
+			return (isKey) ? Activator.getDefault().getImage(Activator.ICON_VALUES_KEY) : Activator.getDefault().getImage(
+					Activator.ICON_VALUES);
 		} else if (element instanceof ScreenIntegerFieldModel) {
+			boolean isKey = ((ScreenIntegerFieldModel)element).isKey();
 			if (!((ScreenFieldModel)element).getValidationMessages().isEmpty()) {
-				return Activator.getDefault().getImage(Activator.ICON_INTEGER_ERR);
+				return (isKey) ? Activator.getDefault().getImage(Activator.ICON_INTEGER_KEY_ERR)
+						: Activator.getDefault().getImage(Activator.ICON_INTEGER_ERR);
 			}
-			return Activator.getDefault().getImage(Activator.ICON_INTEGER);
+			return (isKey) ? Activator.getDefault().getImage(Activator.ICON_INTEGER_KEY) : Activator.getDefault().getImage(
+					Activator.ICON_INTEGER);
 		} else if (element instanceof ScreenPartModel) {
 			List<ScreenFieldModel> sortedFields = ((ScreenPartModel)element).getSortedFields();
 			for (ScreenFieldModel screenFieldModel : sortedFields) {
@@ -64,12 +79,14 @@ public class PartsMasterBlockLabelProvider extends LabelProvider {
 			}
 			return Activator.getDefault().getImage(Activator.ICON_PART);
 		} else if (element instanceof ScreenFieldModel) {
+			boolean isKey = ((ScreenFieldModel)element).isKey();
 			if (!((ScreenFieldModel)element).getValidationMessages().isEmpty()) {
-				return Activator.getDefault().getImage(Activator.ICON_STRING_ERR);
+				return (isKey) ? Activator.getDefault().getImage(Activator.ICON_STRING_KEY_ERR)
+						: Activator.getDefault().getImage(Activator.ICON_STRING_ERR);
 			}
-			return Activator.getDefault().getImage(Activator.ICON_STRING);
+			return (isKey) ? Activator.getDefault().getImage(Activator.ICON_STRING_KEY) : Activator.getDefault().getImage(
+					Activator.ICON_STRING);
 		}
 		return null;
 	}
-
 }
