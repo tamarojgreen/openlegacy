c366b74b2a77b3f60fa7943a4c164732e0fa1e31
roi0507 <roi0507@gmail.com>
2015-02-13 16:03:48 +0200

    refs #731 - enhance generation for screens REST

---
+++ openlegacy-web/src/main/java/org/openlegacy/terminal/mvc/rest/AbstractScreensRestController.java
@@ -0,0 +1,48 @@
+package org.openlegacy.terminal.mvc.rest;
+
+import org.openlegacy.definitions.ActionDefinition;
+import org.openlegacy.mvc.AbstractRestController;
+import org.openlegacy.terminal.ScreenEntity;
+import org.openlegacy.terminal.TerminalSession;
+import org.openlegacy.terminal.actions.TerminalAction;
+import org.openlegacy.terminal.definitions.TerminalActionDefinition;
+import org.openlegacy.terminal.services.ScreenEntitiesRegistry;
+import org.openlegacy.terminal.utils.ScreenEntityUtils;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+public abstract class AbstractScreensRestController extends AbstractRestController {
+
+	@Inject
+	private TerminalSession terminalSession;
+
+	@Inject
+	private ScreenEntitiesRegistry entitiesRegistry;
+
+	@Inject
+	private ScreenEntityUtils screenEntityUtils;
+
+	@Override
+	protected TerminalSession getSession() {
+		return terminalSession;
+	}
+
+	@Override
+	protected ScreenEntitiesRegistry getEntitiesRegistry() {
+		return entitiesRegistry;
+	}
+
+	@Override
+	protected List<ActionDefinition> getActions(Object entity) {
+		// actions for screen exists on the entity. No need to fetch from registry
+		return null;
+	}
+
+	@Override
+	protected Object sendEntity(Object entity, String action) {
+		TerminalActionDefinition actionDefinition = screenEntityUtils.findAction((ScreenEntity)entity, action);
+		return terminalSession.doAction((TerminalAction)actionDefinition.getAction(), (ScreenEntity)entity);
+	}
+}
