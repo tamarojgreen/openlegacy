9583d84f3ed885486c0246809369edddb3a92990
Ivan Bort <ivan.b@openlegacy.com>
2015-04-22 09:38:10 +0300

    refs #1058 - removed opening incorrect emulation url

--- openlegacy-ide-plugins/openlegacy-ide-plugin/openlegacy-ide-eclipse/src/main/java/org/openlegacy/ide/eclipse/actions/RunEmulationAction.java
+++ openlegacy-ide-plugins/openlegacy-ide-plugin/openlegacy-ide-eclipse/src/main/java/org/openlegacy/ide/eclipse/actions/RunEmulationAction.java
@@ -8,19 +8,10 @@ import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.TreeSelection;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.browser.IWebBrowser;
-import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.openlegacy.designtime.enums.BackendSolution;
-import org.openlegacy.designtime.enums.FrontendSolution;
 import org.openlegacy.ide.eclipse.util.PopupUtil;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.text.MessageFormat;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class RunEmulationAction extends AbstractAction {
 
@@ -36,33 +27,31 @@ public class RunEmulationAction extends AbstractAction {
 			if (configFile.exists()) {
 				ILaunchConfiguration configuration = manager.getLaunchConfiguration(configFile);
 				DebugUITools.launch(configuration, ILaunchManager.RUN_MODE);
-				Timer timer = new Timer();
-				timer.schedule(new TimerTask() {
-
-					@Override
-					public void run() {
-						try {
-							IWorkbenchBrowserSupport browserSupport = PlatformUI.getWorkbench().getBrowserSupport();
-							IWebBrowser browser = browserSupport.createBrowser("openlegacy");
-							String frontendSolution = EclipseDesignTimeExecuter.instance().getPreference(project,
-									"FRONTEND_SOLUTION");
-							String urlPart = "";
-							if (frontendSolution.equals(FrontendSolution.REST.name())) {
-								urlPart = "app";
-							} else if (frontendSolution.equals(FrontendSolution.INTEGRATION.name())) {
-								urlPart = "services";
-							}
-							browser.openURL(new URL(MessageFormat.format("http://localhost:1512/{0}/{1}", project.getName(),
-									urlPart)));
-						} catch (PartInitException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (MalformedURLException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}, 13000);
+				//				Timer timer = new Timer();
+				//				timer.schedule(new TimerTask() {
+				//
+				//					@Override
+				//					public void run() {
+				//						try {
+				//							IWorkbenchBrowserSupport browserSupport = PlatformUI.getWorkbench().getBrowserSupport();
+				//							IWebBrowser browser = browserSupport.createBrowser("openlegacy");
+				//							String frontendSolution = EclipseDesignTimeExecuter.instance().getPreference(project,
+				//									"FRONTEND_SOLUTION");
+				//							String urlPart = "";
+				//							if (frontendSolution.equals(FrontendSolution.REST.name())) {
+				//								urlPart = "app";
+				//							} else if (frontendSolution.equals(FrontendSolution.INTEGRATION.name())) {
+				//								urlPart = "services";
+				//							}
+				//							browser.openURL(new URL(MessageFormat.format("http://localhost:1512/{0}/{1}", project.getName(),
+				//									urlPart)));
+				//						} catch (PartInitException e) {
+				//							e.printStackTrace();
+				//						} catch (MalformedURLException e) {
+				//							e.printStackTrace();
+				//						}
+				//					}
+				//				}, 13000);
 			} else {
 				PopupUtil.error(MessageFormat.format("Required launcher file \"{0}\" is missing!", LAUNCHER_FILE_NAME),
 						"Missing file");
