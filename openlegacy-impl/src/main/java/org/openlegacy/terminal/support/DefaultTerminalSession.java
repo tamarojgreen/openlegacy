acaa7b816208c4e4648d1a60e01d5bfa3e9dfc46
roi0507 <roi0507@gmail.com>
2015-05-10 11:13:09 +0300

    refs #1014 - missing in commit

--- openlegacy-impl/src/main/java/org/openlegacy/terminal/support/DefaultTerminalSession.java
+++ openlegacy-impl/src/main/java/org/openlegacy/terminal/support/DefaultTerminalSession.java
@@ -450,7 +450,8 @@ public class DefaultTerminalSession extends AbstractSession implements TerminalS
 
 	@Override
 	public synchronized TerminalSnapshot fetchSnapshot() {
-		return terminalConnection.fetchSnapshot();
+		snapshot = terminalConnection.fetchSnapshot();
+		return snapshot;
 	}
 
 	@Override
