51c7a69a6a6bdc1b18322680b785c965cb13408b
Vlad Gorshkov <vlad.g@openlegacy.com>
2015-05-25 15:46:51 +0300

    refs #1118 - Sample implementation

---
+++ openlegacy/openlegacy-providers/openlegacy-ws-rpc/src/test/java/org/openlegacy/providers/wsrpc/example/Main.java
@@ -0,0 +1,86 @@
+package org.openlegacy.providers.wsrpc.example;
+
+import javax.xml.ws.Endpoint;
+
+public class Main
+{	
+	private static int servicePort = 8080;
+    private static boolean running = false;
+    private static Endpoint endpoints[] = new Endpoint[2];
+    private static Object services[] = {null,null};
+ 
+    public static void main(String args[])
+    {
+        services[0] = new SimpleWebService();
+        services[1] = new NewWebService();
+        try
+        {
+            servicePort = Integer.valueOf(args[0]).intValue();
+        }
+        catch(Exception exception) { }
+        publish();
+        String param = "";
+        while (true){
+        	System.out.print("command:");
+            param = System.console().readLine();
+            
+            if(param.equals("publish"))
+                publish();
+            
+            if(param.equals("stop"))            
+                stop();                
+            
+            if(param.equals("port")){
+            	try
+                {
+                    System.out.print("port:");
+                    servicePort = Integer.valueOf(System.console().readLine()).intValue();
+                    stop();
+                    publish();
+                }
+                catch(Exception e) { }	
+            }
+            
+            if(param.equals("exit")){
+            	stop();
+            	break;
+            }
+        }   
+    }
+
+    public static void publish()
+    {
+        if(running)
+            return;
+        for(int i = 0; i < endpoints.length; i++)
+        {
+            endpoints[i] = Endpoint.create(services[i]);
+            try
+            {
+                String path = String.format("http://localhost:%d/%s", servicePort,services[i].getClass().getSimpleName());
+                endpoints[i].publish(path);
+                System.out.println(String.format("Service was started at: %s", path));
+                running = true;
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+                System.console().readLine();
+                return;
+            }
+        }
+    }
+
+    public static void stop()
+    {
+        if(!running)
+            return;
+        for(int i = 0; i < endpoints.length; i++)
+            endpoints[i].stop();
+
+        System.out.println("Service was stopped");
+        running = false;
+    }
+    
+
+}
---
+++ openlegacy/openlegacy-providers/openlegacy-ws-rpc/src/test/java/org/openlegacy/providers/wsrpc/example/SimpleWebService.java
@@ -0,0 +1,27 @@
+package org.openlegacy.providers.wsrpc.example;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService(name="SimpleWebService", targetNamespace="http://SimpleWebService/")
+@SOAPBinding(style=Style.RPC)
+public class SimpleWebService
+{
+    @WebMethod
+    @WebResult(partName="callBackResult")
+    public String callBackString(@WebParam(name="callBackValue") String callBackValue)
+    {
+        return callBackValue;
+    }
+
+    @WebMethod
+    @WebResult(partName="callBackResult")
+    public int callBackInteger(@WebParam(name="callBackValue") int callBackValue)
+    {
+        return callBackValue;
+    }
+}
