595d10ad2d640ceef40389d52ba9aa7883283100
Vlad Gorshkov <vlad.g@openlegacy.com>
2015-05-25 15:46:51 +0300

    refs #1118 - Sample implementation

---
+++ openlegacy/openlegacy-providers/openlegacy-ws-rpc/src/test/java/org/openlegacy/providers/wsrpc/example/NewWebService.java
@@ -0,0 +1,15 @@
+package org.openlegacy.providers.wsrpc.example;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+
+@WebService
+public class NewWebService{
+
+    @WebMethod
+    public String getHelloWorld()
+    {
+        return "Hello World";
+    }
+}
