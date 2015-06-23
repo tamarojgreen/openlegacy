47eb526b9c13e6a688bf5190eeb108185279a5fc
Vlad Gorshkov <vlad.g@openlegacy.com>
2015-06-23 22:13:01 +0300

    Corrected RpcTableUtil for TOP inner list

--- openlegacy-impl/src/main/java/org/openlegacy/rpc/modules/table/RpcTableUtil.java
+++ openlegacy-impl/src/main/java/org/openlegacy/rpc/modules/table/RpcTableUtil.java
@@ -8,6 +8,7 @@ import org.openlegacy.rpc.definitions.RpcPartEntityDefinition;
 import org.openlegacy.rpc.utils.SimpleRpcPojoFieldAccessor;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 public class RpcTableUtil {
@@ -17,12 +18,48 @@ public class RpcTableUtil {
 		for (Entry<String, PartEntityDefinition<RpcFieldDefinition>> item : rpcEntityDefinition.getPartsDefinitions().entrySet()) {
 			RpcPartEntityDefinition rpcPart = (RpcPartEntityDefinition)item.getValue();
 
-			if (rpcPart.getCount() > 0) {
-				SimpleRpcPojoFieldAccessor fieldAccessor = new SimpleRpcPojoFieldAccessor(rpcEntity);
-				return (List<?>)fieldAccessor.getFieldValue(rpcPart.getPartName());
+			Object obj = null;
+			try {
+				if (rpcPart.getCount() > 0) {
+					SimpleRpcPojoFieldAccessor fieldAccessor = new SimpleRpcPojoFieldAccessor(rpcEntity);
+					obj = fieldAccessor.getFieldValue(rpcPart.getPartName());
+					if (obj instanceof List<?>) {
+						return (List<?>)obj;
+					} else {
+						obj = findInnerPartList(obj, rpcPart.getInnerPartsDefinitions());
+						if (obj != null) {
+							return (List<?>)obj;
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
+		return null;
+	}
 
+	public static List<?> findInnerPartList(Object parent, Map<String, RpcPartEntityDefinition> inner) {
+		for (Entry<String, RpcPartEntityDefinition> item : inner.entrySet()) {
+			RpcPartEntityDefinition rpcPart = item.getValue();
+			Object obj = null;
+			try {
+				if (rpcPart.getCount() > 0) {
+					SimpleRpcPojoFieldAccessor fieldAccessor = new SimpleRpcPojoFieldAccessor(parent);
+					obj = fieldAccessor.getFieldValue(rpcPart.getPartName());
+					if (obj instanceof List<?>) {
+						return (List<?>)obj;
+					} else {
+						obj = findInnerPartList(obj, rpcPart.getInnerPartsDefinitions());
+						if (obj != null) {
+							return (List<?>)obj;
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		return null;
 	}
 }
