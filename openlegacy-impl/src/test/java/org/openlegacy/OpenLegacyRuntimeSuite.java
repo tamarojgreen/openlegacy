6904b46c4320ea7595b8e6b163781acde8e0c843
Vlad Gorshkov <vlad.g@openlegacy.com>
2015-08-10 16:00:58 +0300

    refs #1228 - wsRegistry test added to runtime suite

--- openlegacy-impl/src/test/java/org/openlegacy/OpenLegacyRuntimeSuite.java
+++ openlegacy-impl/src/test/java/org/openlegacy/OpenLegacyRuntimeSuite.java
@@ -6,9 +6,11 @@ import org.junit.runners.Suite.SuiteClasses;
 import org.openlegacy.db.OpenLegacyDbRuntimeSuite;
 import org.openlegacy.rpc.OpenLegacyRpcRuntimeSuite;
 import org.openlegacy.terminal.OpenLegacyScreensRuntimeSuite;
+import org.openlegacy.ws.WebServiceRegistryTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ OpenLegacyScreensRuntimeSuite.class, OpenLegacyRpcRuntimeSuite.class, OpenLegacyDbRuntimeSuite.class })
+@SuiteClasses({ OpenLegacyScreensRuntimeSuite.class, OpenLegacyRpcRuntimeSuite.class, OpenLegacyDbRuntimeSuite.class,
+		WebServiceRegistryTest.class })
 public class OpenLegacyRuntimeSuite {
 
 }
