0925103812d5ff2e0fbeb8c889090156197bd823
Vlad Gorshkov <vlad.g@openlegacy.com>
2015-08-20 14:11:29 +0300

    refs #1228 - Implemented proxy creator for proxying ws beans only

---
+++ openlegacy-impl/src/main/java/org/openlegacy/aop/ws/WebServiceBeanNameAutoProxyCreator.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2015 OpenLegacy Inc.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     OpenLegacy Inc. - initial API and implementation
+ *******************************************************************************/
+
+package org.openlegacy.aop.ws;
+
+import org.openlegacy.aop.OpenLegacyBeanNameAutoProxyCreator;
+
+public class WebServiceBeanNameAutoProxyCreator extends OpenLegacyBeanNameAutoProxyCreator {
+
+	private static final long serialVersionUID = 1L;
+
+}
