449563c0ff5f0bc6623afe5919fbd6014f7d6870
dganitd <dganit.david@gmail.com>
2015-09-10 14:58:59 +0300

    fix test for openlegacy-jt400.

--- openlegacy/openlegacy-providers/openlegacy-jt400/src/test/java/org/openlegacy/providers/jt400/Jt400OnLineSuite.java
+++ openlegacy/openlegacy-providers/openlegacy-jt400/src/test/java/org/openlegacy/providers/jt400/Jt400OnLineSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ Jt400OnLineSuite.class, Jt400OffLineSuite.class })
-public class Jt400Suite {
+@SuiteClasses({ Jt400RpcConnectionTest.class, Jt400RpcSessionTest.class, Jt400RpcManipulateSessionTest.class })
+public class Jt400OnLineSuite {
 
 }
