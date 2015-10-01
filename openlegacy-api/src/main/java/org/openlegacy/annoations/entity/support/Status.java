3cd4b62326b902d7b781ace3282f2d79b63b4a46
Vlad Gorshkov <vlad.g@openlegacy.com>
2015-10-01 14:22:38 +0300

    refs #1279 - Implemented entity status saver

--- openlegacy-api/src/main/java/org/openlegacy/annoations/entity/support/Status.java
+++ openlegacy-api/src/main/java/org/openlegacy/annoations/entity/support/Status.java
@@ -11,6 +11,13 @@
 
 package org.openlegacy.annoations.entity.support;
 
+import org.openlegacy.annotations.entity.Active;
+import org.openlegacy.annotations.entity.Deprecated;
+import org.openlegacy.annotations.entity.Invalid;
+import org.openlegacy.annotations.entity.Paused;
+
+import java.lang.annotation.Annotation;
+
 public enum Status {
 	DEFAULT,
 	ACTIVE,
@@ -32,5 +39,31 @@ public enum Status {
 
 	public boolean isLoggable() {
 		return this == DEPRECATED;
+	}
+
+	public String getAsString() {
+		switch (this) {
+			case DEPRECATED:
+				return Deprecated.class.getSimpleName();
+			case INVALID:
+				return Invalid.class.getSimpleName();
+			case PAUSED:
+				return Paused.class.getSimpleName();
+			default:
+				return Active.class.getSimpleName();
+		}
+	}
+
+	public Class<? extends Annotation> getAsClass() {
+		switch (this) {
+			case DEPRECATED:
+				return Deprecated.class;
+			case INVALID:
+				return Invalid.class;
+			case PAUSED:
+				return Paused.class;
+			default:
+				return Active.class;
+		}
 	}
 }
