4e4e057eaae21ce293feacb2e8927df2ca74b405
Vlad Gorshkov <vlad.g@openlegacy.com>
2015-10-15 15:39:20 +0300

    refs #1279 - Added missed doAction method interception

--- openlegacy-impl/src/main/java/org/openlegacy/support/SessionEntityStatusProcessor.java
+++ openlegacy-impl/src/main/java/org/openlegacy/support/SessionEntityStatusProcessor.java
@@ -13,13 +13,18 @@ package org.openlegacy.support;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.lang.ClassUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openlegacy.EntitiesRegistry;
+import org.openlegacy.db.DbEntity;
 import org.openlegacy.definitions.support.AbstractEntityDefinition;
 import org.openlegacy.exceptions.OpenLegacyRuntimeException;
+import org.openlegacy.rpc.RpcEntity;
+import org.openlegacy.terminal.ScreenEntity;
 
 import java.lang.reflect.Method;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -28,6 +33,7 @@ public class SessionEntityStatusProcessor implements MethodInterceptor {
 	private static final String MESSAGE = "Entity %s with %s status used in %s session.";
 	private static final String VERIFY = " Verify it usage.";
 	private static final String GET_ENTITY = "getEntity";
+	private static final String DO_ACTION = "doAction";
 
 	private static final Log logger = LogFactory.getLog(SessionEntityStatusProcessor.class);
 
@@ -38,10 +44,13 @@ public class SessionEntityStatusProcessor implements MethodInterceptor {
 	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
 		Method method = invocation.getMethod();
+		boolean isDoAction = false;
 
-		if (!method.getName().equals(GET_ENTITY)) {
+		if (!(method.getName().equals(GET_ENTITY) || method.getName().equals(DO_ACTION))) {
 			return invocation.proceed();
 		}
+
+		isDoAction = method.getName().equals(DO_ACTION);
 
 		Object[] arguments = invocation.getArguments();
 
@@ -49,7 +58,21 @@ public class SessionEntityStatusProcessor implements MethodInterceptor {
 			return invocation.proceed();
 		}
 
-		AbstractEntityDefinition<?> entityDef = getEntityDefinition(arguments[0]);
+		if (isDoAction && arguments.length == 1) {
+			return invocation.proceed();
+		}
+
+		if (isDoAction) {
+			@SuppressWarnings("unchecked")
+			List<Class<?>> interfaces = ClassUtils.getAllInterfaces(arguments[1].getClass());
+			if (!(interfaces.contains(RpcEntity.class) || interfaces.contains(ScreenEntity.class) || interfaces.contains(DbEntity.class))) {
+				return invocation.proceed();
+			}
+		}
+
+		AbstractEntityDefinition<?> entityDef = isDoAction ? getEntityDefinition(arguments[1])
+				: getEntityDefinition(arguments[0]);
+
 		Class<?> session = method.getDeclaringClass();
 		String message = String.format(MESSAGE, entityDef.getEntityName(), entityDef.getStatus().getAsString(),
 				session.getSimpleName());
@@ -68,8 +91,12 @@ public class SessionEntityStatusProcessor implements MethodInterceptor {
 
 	@SuppressWarnings("unchecked")
 	private AbstractEntityDefinition<?> getEntityDefinition(Object firstArgument) {
-		return (AbstractEntityDefinition<?>)(firstArgument instanceof Class ? registry.get((Class<?>)firstArgument)
-				: registry.get((String)firstArgument));
+		if (firstArgument instanceof Object) {
+			return (AbstractEntityDefinition<?>)registry.get(firstArgument.getClass());
+		} else {
+			return (AbstractEntityDefinition<?>)(firstArgument instanceof Class ? registry.get((Class<?>)firstArgument)
+					: registry.get((String)firstArgument));
+		}
 	}
 
 }
