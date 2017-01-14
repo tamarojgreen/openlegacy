1d074d63e2222714a1aeae21a8b7b1bc39b1a256
Tamaro J Green <tamarojgreen@localhost.localdomain>
2017-01-14 12:04:55 -0600

    Testing Spark Framework

--- openlegacy-web/src/main/java/org/openlegacy/rpc/spa/AngularRpcController.java
+++
@@ -1,9 +0,0 @@
-package org.openlegacy.rpc.spa;
-
-import org.openlegacy.spa.AbstractAngularController;
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class AngularRpcController extends AbstractAngularController {
-
-}
