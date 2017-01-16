ed86fbf09a67d9c4628aef66163a4349c5517f3f
Tamaro Green <tamarojgreen@hotmail.com>
2017-01-16 13:57:59 -0600

    Cleaning commented code

--- openlegacy-web/src/main/java/org/openlegacy/Main.java
+++ openlegacy-web/src/main/java/org/openlegacy/Main.java
@@ -1,11 +1,6 @@
 package org.openlegacy;
 
 import static spark.Spark.*;
-import static spark.Spark.get;
-import static spark.Spark.post;
-import spark.Request;
-import spark.Response;
-import spark.Route;
 
 
 public class Main {
